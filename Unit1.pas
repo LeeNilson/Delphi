f6109d5968c5c025a485dd6eebf7d4f2de3f6afc
Lee <99687743+LeeNilson@users.noreply.github.com>
2025-09-26 22:20:22 -0300

    Add product and client registration forms
    Created new forms for product (Unit1) and client (Unit2) registration, including their DFM and PAS files. Updated the project file to include these forms. Added initial project configuration and history/recovery files.

---
+++ Unit1.pas
@@ -0,0 +1,32 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TFrmCadastroProdutos = class(TForm)
+    edtProduto: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    edtQuantidadeProduto: TEdit;
+    Label3: TLabel;
+    edtValorProduto: TEdit;
+    edtcategoria: TEdit;
+    Label4: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmCadastroProdutos: TFrmCadastroProdutos;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ __recovery/Unit3.pas
@@ -0,0 +1,35 @@
+unit Unit3;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Menus, Vcl.ExtCtrls;
+
+type
+  TfrmVendas = class(TForm)
+    Panel1: TPanel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Edit4: TEdit;
+    Edit3: TEdit;
+    ComboBox2: TComboBox;
+    Label4: TLabel;
+    MainMenu1: TMainMenu;
+    CadastroCliente1: TMenuItem;
+    CadastroProduto1: TMenuItem;
+    CadastroVenda1: TMenuItem;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmVendas: TfrmVendas;
+
+implementation
+
+{$R *.dfm}
+
+end.
